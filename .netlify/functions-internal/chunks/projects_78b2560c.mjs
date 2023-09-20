a202e0ddf5c80c55bd17639a9fff9c73de716304
Derick <derickchoskinson@gmail.com>
2023-09-20 05:16:24 -0500

    updates

---
+++ .netlify/functions-internal/chunks/projects_78b2560c.mjs
@@ -0,0 +1,11 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import 'react';
+import 'react-dom/server';
+import './astro_63bc8839.mjs';
+import 'clsx';
+import 'html-escaper';
+
+const page = () => import('./pages/projects_e006dfb4.mjs');
+
+export { page };
