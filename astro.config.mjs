09bc1eb1409b1c2655345619305043b6aacb954a
Derick <derickchoskinson@gmail.com>
2023-09-20 04:01:18 -0500

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cellularwind.com/",
   integrations: [mdx(), sitemap(), tailwind(), react()],
+  output: "server",
+  adapter: netlify()
 });
